2ef4bbb326b2a5a02496431f996b0159a9727387
Radzia <radek.chojnowski@gmail.com>
2018-03-07 22:10:56 +0100

    pliki

---
+++ script1.js
@@ -0,0 +1,8 @@
+function klik(){
+			var name = document.getElementById("name");
+			var div = document.getElementById("out");
+		
+			div.innerHTML = "witaj urzytkowniku :" + name.value;
+			
+			
+			}
